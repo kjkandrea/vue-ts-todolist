1ee071ef7db0c91a61deec00b01c3a040a4b3edc
Jongkeun Kim <kjkandrea@gmail.com>
2020-09-05 18:21:56 +0900

    itemInput addItem 기능 구현

--- src/components/ItemInput.vue
+++ src/components/ItemInput.vue
@@ -2,18 +2,37 @@
   <div class="form-group">
     <input type="text"
            class="form-control"
+           v-model="title"
            @keyup.enter="addItem"
-           placeholder="이메일을 입력하세요">
+           placeholder="할일을 입력하세요">
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { mapState } from 'vuex';
 
-@Component
+@Component({
+  computed: {
+    ...mapState([
+        'todoList',
+    ]),
+    lastId(): number {
+      return this.todoList[this.todoList.length - 1].id;
+    },
+  },
+})
 export default class ItemInput extends Vue {
-  public addItem() {
+  public title: string = '';
 
+  public addItem($event: Event) {
+    this.$store.commit('addItem', {
+      id: this.lastId + 1,
+      title: this.title,
+      status: 'active',
+    });
+
+    $event.target.value = '';
   }
 }
 </script>
